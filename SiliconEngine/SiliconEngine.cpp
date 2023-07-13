112e16b0e596b9edb5cd0ea8a763767440a0b7f3
zachycardia <zachycardian@gmail.com>
2023-07-14 02:54:55 +0800

    Initial commit

---
+++ SiliconEngine/SiliconEngine.cpp
@@ -0,0 +1,12 @@
+﻿// SiliconEngine.cpp : Defines the entry point for the application.
+//
+
+#include "SiliconEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
