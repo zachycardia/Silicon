54fcb73f4f903a7f04de8b459228892f489b3841
zachycardia <zachycardian@gmail.com>
2023-07-17 09:13:50 +0800

    Add SiliconTest subproject

---
+++ SiliconTest/src/main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file    main.cpp
+ *
+ * @brief   Program entry point.
+ *          Used for debugging purposes.
+ * 
+ * @author  Zachary Ning
+**/
+
+#include "silicon_engine.h"
+
+int main() {
+	Silicon::Engine engine{};
+
+	return 0;
+}
