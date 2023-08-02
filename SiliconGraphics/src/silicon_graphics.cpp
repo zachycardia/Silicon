15fbe49ef0555a36bb2da3aedf80a00ed5008501
zachycardia <zachycardian@gmail.com>
2023-08-03 03:24:56 +0800

    Add Vulkan library to SiliconGraphics

--- SiliconGraphics/src/silicon_graphics.cpp
+++ SiliconGraphics/src/silicon_graphics.cpp
@@ -1,9 +1,16 @@
 #include "silicon_graphics.h"
 
+#include <vulkan/vulkan.h>
+
+#include "logging/logger.h"
+
 namespace Silicon {
 
 void Graphics::Initialize() {
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
+	Logger::Instance().Debug("Extensions: {0}", extensionCount);
 }
 
 void Graphics::Destroy() {
