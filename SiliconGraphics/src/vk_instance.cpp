9955f368e3adfaf4c259fa0e3bf972a2fa81d7bb
zachycardia <zachycardian@gmail.com>
2023-08-04 23:17:39 +0800

    Implement validation layers

--- SiliconGraphics/src/vk_instance.cpp
+++ SiliconGraphics/src/vk_instance.cpp
@@ -1,6 +1,7 @@
 #include "vk_instance.h"
 
-#include <vector>
+#include <algorithm>
+#include <cstring>
 #include <vulkan/vulkan.h>
 
 #include "common/vulkan_assert.h"
@@ -26,12 +27,54 @@ void Vk_Instance::Create(InstanceCreateInfo info) {
 		.ppEnabledExtensionNames = info.extensionNames
 	};
 
+	// Validation layers.
+	if (c_enableValidationLayers && !HasValidationLayerSupport()) {
+		Logger::Instance().Error("Requested validation layers are not available.");
+	}
+	if (c_enableValidationLayers) {
+		createInfo.enabledLayerCount = static_cast<uint32_t>(c_validationLayers.size());
+		createInfo.ppEnabledLayerNames = c_validationLayers.data();
+	} else {
+		createInfo.enabledLayerCount = 0;
+	}
+
+	// Create instance.
 	VkAssert(vkCreateInstance(&createInfo, nullptr, &m_instance),
 		"Failed to create instance.");
+
+	// Initialize device.
+	m_device.Initialize(m_instance);
 }
 
 void Vk_Instance::Destroy() {
+	m_device.Destroy();
 	vkDestroyInstance(m_instance, nullptr);
+}
+
+void Vk_Instance::InitializeValidationLayer() {
+}
+
+bool Vk_Instance::HasValidationLayerSupport() {
+	// Get available layers.
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	// Check if all validation layers are available.
+	for (const char* i : c_validationLayers) {
+		bool found = std::find_if(availableLayers.begin(), availableLayers.end(),
+			[&i](VkLayerProperties prop) -> bool {
+				return strcmp(i, prop.layerName) == 0;
+			}
+		) != availableLayers.end();
+
+		if (!found) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
 }
--- SiliconGraphics/src/vk_instance.h
+++ SiliconGraphics/src/vk_instance.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <vulkan/vulkan.h>
 
 namespace Silicon {
@@ -18,8 +19,21 @@ public:
 	void Destroy();
 
 private:
+	const std::vector<const char*> c_validationLayers = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+#ifdef NDEBUG
+	static constexpr bool c_enableValidationLayers = false;
+#else
+	static constexpr bool c_enableValidationLayers = true;
+#endif
 
 	VkInstance m_instance;
+
+	void InitializeValidationLayer();
+
+	bool HasValidationLayerSupport();
+
 };
 
 }
